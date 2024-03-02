98e680db9b13912c012c1b1a20d755face4bfa9a
MrCodeBS <singhbhupider206@gmail.com>
2024-03-02 20:25:57 +0100

    done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "N-gge-"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
